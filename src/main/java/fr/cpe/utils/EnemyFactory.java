c2ab89585322a9f25a12eff65f811c81a24ffa5e
Eloi Villette <eloi.villette@outlook.fr>
2026-03-31 17:23:52 +0200

    Amélioration de la factory pour gérer différant vaisseau

--- src/main/java/fr/cpe/utils/EnemyFactory.java
+++ src/main/java/fr/cpe/utils/EnemyFactory.java
@@ -2,11 +2,21 @@ package fr.cpe.utils;
 
 import fr.cpe.model.ClassicEnemy;
 import fr.cpe.model.Enemy;
-
+import fr.cpe.model.HeavyEnemy;
+import fr.cpe.model.LightEnemy;
+import java.util.Random;
 
 public class EnemyFactory {
 
-    public Enemy CreateEnemy(double x, double y) {
-        return new ClassicEnemy(0, 0, 0, 0);
+    static public Enemy CreateEnemy(double x, double y, int direction, int speed) {
+        Random random = new Random(System.currentTimeMillis());
+        int rand = random.nextInt(3) + 1;
+
+        return switch (rand) {
+            case 1 -> new ClassicEnemy(0, 0, 0, 0);
+            case 2 -> new LightEnemy(0, 0, 0, 0);
+            case 3 -> new HeavyEnemy(0, 0, 0, 0);
+            default -> null;
+        };
     }
 }
