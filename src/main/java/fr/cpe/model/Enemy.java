0db1eb17cdfa426a09603c5416edaa429e65559c
Eloi Villette <eloi.villette@outlook.fr>
2026-04-01 11:21:59 +0200

    Ajoute des HP, HPMax et reloadTime pour les Enemy

--- src/main/java/fr/cpe/model/Enemy.java
+++ src/main/java/fr/cpe/model/Enemy.java
@@ -1,8 +1,13 @@
 package fr.cpe.model;
 
 import fr.cpe.utils.EnemyStrategy;
-abstract public class Enemy extends SpacialObject{
+
+abstract public class Enemy extends SpacialObject implements EnemyStrategy {
     public EnemyStrategy strategy;
+    public int hp;
+    public int maxHp;
+    public int reloadTime;
+
     public Enemy(double x, double y, int direction, int speed) {
         super(x, y, direction, speed);
     }
