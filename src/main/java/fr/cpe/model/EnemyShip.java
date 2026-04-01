39e1618a4b6d69ade8c0cc5b0c2fa4ff376d1826
Timothee754 <timotheepierre.rey@gmail.com>
2026-04-01 17:34:56 +0200

    Supprimer mauvais implement d'une strategy

--- src/main/java/fr/cpe/model/EnemyShip.java
+++ src/main/java/fr/cpe/model/EnemyShip.java
@@ -2,14 +2,11 @@ package fr.cpe.model;
 
 import fr.cpe.utils.EnemyStrategy;
 
-abstract public class EnemyShip extends Ship implements EnemyStrategy {
+abstract public class EnemyShip extends Ship {
     public EnemyStrategy strategy;
 
     public EnemyShip(double x, double y, double direction, int speed, int hp, int maxHp, int reloadTime, EnemyStrategy strategy) {
         super(x, y, direction, speed, hp, maxHp, reloadTime);
         this.strategy = strategy;
     }
-
-
-    public abstract String getType();
 }
