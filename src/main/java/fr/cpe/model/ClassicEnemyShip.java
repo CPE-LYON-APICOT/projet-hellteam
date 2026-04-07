85e6e04f66e59cf117792447e9112e1edbf63c97
Eloi Villette <eloi.villette@outlook.fr>
2026-04-07 17:15:52 +0200

    refacto de direction par angle

--- src/main/java/fr/cpe/model/ClassicEnemyShip.java
+++ src/main/java/fr/cpe/model/ClassicEnemyShip.java
@@ -3,8 +3,8 @@ package fr.cpe.model;
 import fr.cpe.utils.EnemyStrategy;
 
 public class ClassicEnemyShip extends EnemyShip {
-    public ClassicEnemyShip(double x, double y, int direction, int speed, int hp, int maxHp, int reloadTime, EnemyStrategy strategy) {
-        super(x, y, direction, speed, hp, maxHp, reloadTime, strategy);
+    public ClassicEnemyShip(double x, double y, int angle, int speed, int hp, int maxHp, int reloadTime, EnemyStrategy strategy) {
+        super(x, y, angle, speed, hp, maxHp, reloadTime, strategy);
     }
 
     @Override
